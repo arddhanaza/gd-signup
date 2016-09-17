65a70338a878953042260bcec4d35ba5e85f48f4
Arddhana Zhafran Amanullah <arddhanaza99@gmail.com>
2016-09-17 21:14:31 +0700

    + penambahan validasi dari setiap widget dari nama, email, jenis kelamin dan checkbox.

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl1003/gdsignup/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl1003/gdsignup/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -42,6 +43,62 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void doClick() {
+        String nama = etNama.getText().toString();
+        String email = etEmail.getText().toString();
+        String hasil = null;
+        String hasilcb = "\nSoftware yang Anda Kuasai: \n";
+        int startlen = hasilcb.length();
 
+        if (isValid()) {
+
+            if (rgKelas.getCheckedRadioButtonId() != -1) {
+                RadioButton rb = (RadioButton)
+                        findViewById(rgKelas.getCheckedRadioButtonId());
+                hasil = rb.getText().toString();
+            }
+            if (hasil == null) {
+                tvHasil.setText("Jenis Kelamin Belum Diisi");
+            } else {
+                if (cbAI.isChecked()) hasilcb += cbAI.getText() + "\n";
+                if (cbCD.isChecked()) hasilcb += cbCD.getText() + "\n";
+                if (cbPS.isChecked()) hasilcb += cbPS.getText() + "\n";
+                if (hasilcb.length() == startlen) hasilcb += "Tidak ada Pilihan";
+
+                tvHasil.setText("Nama Anda: " + nama + "\n\nEmail Anda: " + email + "\n\nKelas: " + (spKelas.getSelectedItem().toString()) + "\n\nJenis Kelamin: " + hasil + "\n" + hasilcb);
+            }
+
+
+        }
     }
+
+    private boolean isValid() {
+        boolean valid = true;
+        String nama = etNama.getText().toString();
+        String email = etEmail.getText().toString();
+        final EditText emailval = (EditText) findViewById(R.id.editTextEmail);
+        String email2 = emailval.getText().toString().trim();
+        String emailpattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+
+        if (nama.isEmpty()) {
+            etNama.setError("Nama Belum Diisi");
+            valid = false;
+        } else if (nama.length() < 3) {
+            etNama.setError("Nama Minimal 3 Karakter");
+            valid = false;
+        } else {
+            etNama.setError(null);
+        }
+        if (email.isEmpty()) {
+            etEmail.setError("Email Anda Belum Diisi");
+            valid = false;
+        } else if (!email2.matches(emailpattern)) {
+            etEmail.setError("Silakan Cek Kembali Email Anda");
+            valid = false;
+        } else {
+            etEmail.setError(null);
+        }
+        return valid;
+    }
+
 }
